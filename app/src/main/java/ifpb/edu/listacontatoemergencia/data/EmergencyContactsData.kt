0f77ad7c7002ce1d3d688d09bf96d215abb5b846
YtalloPereira <ytallopereiralves@gmail.com>
2024-12-12 12:48:16 -0300

    :sparkles: feat: adicionando mais dados emergencycontactsdata

--- app/src/main/java/ifpb/edu/listacontatoemergencia/data/EmergencyContactsData.kt
+++ app/src/main/java/ifpb/edu/listacontatoemergencia/data/EmergencyContactsData.kt
@@ -7,22 +7,31 @@ val emergencyContacts = listOf(
     ContactCategory(
         "Federal",
         listOf(
-            EmergencyContact("Polícia Federal", "191"),
-            EmergencyContact("Rodoviária Federal", "191")
+            EmergencyContact("Polícia Federal", "194"),
+            EmergencyContact("Rodoviária Federal", "191"),
+            EmergencyContact("IBAMA (Instituto Brasileiro do Meio Ambiente)", "0800-618080"),
+            EmergencyContact("Receita Federal", "146"),
+            EmergencyContact("Aeronáutica (Polícia)", "190")
         )
     ),
     ContactCategory(
         "Estadual",
         listOf(
             EmergencyContact("Polícia Militar", "190"),
-            EmergencyContact("Bombeiros", "193")
+            EmergencyContact("Bombeiros", "193"),
+            EmergencyContact("Polícia Civil", "197"),
+            EmergencyContact("DETRAN (Departamento Estadual de Trânsito)", "0800-888-2368"),
+            EmergencyContact("Polícia Rodoviária Estadual", "198")
         )
     ),
     ContactCategory(
         "Municipal",
         listOf(
             EmergencyContact("Guarda Municipal", "153"),
-            EmergencyContact("Defesa Civil", "199")
+            EmergencyContact("Defesa Civil", "199"),
+            EmergencyContact("SAMU (Serviço de Atendimento Móvel de Urgência)", "192"),
+            EmergencyContact("Procon Municipal", "151"),
+            EmergencyContact("CET (Companhia de Engenharia de Tráfego)", "156")
         )
     )
 )
